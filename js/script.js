32cbc19ebfe0403770c5e5ffbcce1f3dfd262885
Umar <example@gmail.com>
2023-11-19 18:49:46 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+// alert("hello java script")
+
+// const plase = confirm("Where are you from");
+// console.log(plase);
+
+const currentPlace = prompt("Where are you", "");
+console.log(typeof(currentPlace));
